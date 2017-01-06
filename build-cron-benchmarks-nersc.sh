c144ad20e110f59f6a74a7868807dcf58354f664
Glenn K. Lockwood <glock@lbl.gov>
2017-01-05 16:18:09 -0800

    we can now build h5part on NERSC systems (Cori + Edison)

--- build-cron-benchmarks-nersc.sh
+++ build-cron-benchmarks-nersc.sh
@@ -2,11 +2,29 @@
 
 basedir=$(readlink -f $(dirname "$0"))
 
-# build IOR
-cd $basedir/ior
-mkdir -p build
-./bootstrap
-cd build
-../configure --prefix=$basedir/ior/install --without-gpfs
-make install
+if [ "x$1" == "x" ]; then
+    target="all"
+else
+    target="$1"
+fi
 
+# build IOR
+if [ "$target" == "ior" -o "$target" == "all" ]; then
+    cd $basedir/ior
+    mkdir -p build
+    ./bootstrap
+    cd build
+    ../configure --prefix=$basedir/ior/install --without-gpfs
+    make install
+fi
+
+# build H5Part (required by VPIC-IO and BD-CATS-IO)
+if [ "$target" == "h5part" -o "$target" == "all" ]; then
+    module load cray-hdf5-parallel ### assume HDF5 1.8 or higher
+    cd $basedir/h5part
+    mkdir -p build
+    ./bootstrap
+    cd build
+    ../configure --prefix=$basedir/h5part/install --enable-parallel
+    make install
+fi
