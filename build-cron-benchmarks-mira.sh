56fe0dc961d241d278724b4adfac832a516bbd76
Shane Snyder <ssnyder@mcs.anl.gov>
2017-01-06 15:53:49 +0000

    ior configure typo in mira build script

--- build-cron-benchmarks-mira.sh
+++ build-cron-benchmarks-mira.sh
@@ -7,6 +7,6 @@ cd $basedir/ior
 mkdir -p build
 ./bootstrap
 cd build
-../configure --prefix=$basedir/ior/install --host=powerpc-bgp-linux
+../configure --prefix=$basedir/ior/install --host=powerpc-bgq-linux
 make install
 
