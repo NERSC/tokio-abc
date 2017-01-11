2d3f081579c91651e3641493002d068aa043527a
Kevin Harms <harms@alcf.anl.gov>
2017-01-11 16:31:40 +0000

    Move job working dir for jenkins script to project fs

--- run-jenkins-mira.sh
+++ run-jenkins-mira.sh
@@ -13,20 +13,24 @@ soft add +mpiwrapper-xl.legacy
 if [ $? -eq 0 ];
 then
   # submit to cobale
-  jid=$(qsub -A radix-io -n 2048 -t 30 --mode script --env SCRATCH=/projects/radix-io/automated --run_project ./run-cron-benchmarks-mira.sh)
+  jid=$(qsub --cwd /projects/radix-io/automated/runs -A radix-io -n 2048 -t 30 --mode script --env SCRATCH=/projects/radix-io/automated --run_project ./run-cron-benchmarks-mira.sh)
+  echo "Running as job: $jid"
   cqwait $jid
   # check error code
   exit_code=$(sed -n 's/.* exit code of \([0-9]\); initiating job cleanup and removal/\1/p' ${jid}.cobaltlog)
   if [ -n $exit_code  ] && [ "$ec" = "0" ]; then
     # return zero if the test script returned zero
     rc=0
+    echo "Job succeeded: $rc"
   else
     # return something? if the test script failed
     rc=1
+    echo "Job failed with: $ec"
   fi
 else
   # return the failure from the build script
   rc=$?
+  echo "Build failed: $rc"
 fi
 
 exit $rc
