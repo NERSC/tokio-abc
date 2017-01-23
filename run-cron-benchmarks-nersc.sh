2f4dc3566ebce53e3e81130b89dd06ea6b043672
Glenn K. Lockwood <glock@lbl.gov>
2017-01-23 10:35:15 -0800

    fixed edison static script

--- run-cron-benchmarks-nersc.sh
+++ run-cron-benchmarks-nersc.sh
@@ -15,57 +15,53 @@
 #SBATCH -t 04:00:00
 #SBATCH --mail-user=glock@lbl.gov --mail-type=END
 
-REPO_BASE=$SLURM_SUBMIT_DIR
-
 module load lustre-cray_ari_s
 
 function run_edison() {
+REPO_BASE="/global/u2/g/glock/src/git/tokio-paper-edison"
+
 mkdir -p /scratch1/scratchdirs/$USER/striped
 lfs setstripe -c -1 /scratch1/scratchdirs/$USER/striped
 mkdir -p /scratch2/scratchdirs/$USER/striped
 lfs setstripe -c -1 /scratch2/scratchdirs/$USER/striped
 mkdir -p /scratch3/scratchdirs/$USER/striped
 lfs setstripe -c -1 /scratch3/scratchdirs/$USER/striped
-
 mkdir -p /scratch1/scratchdirs/$USER/nostriped
 lfs setstripe -c 1 /scratch1/scratchdirs/$USER/nostriped
 mkdir -p /scratch2/scratchdirs/$USER/nostriped
 lfs setstripe -c 1 /scratch2/scratchdirs/$USER/nostriped
 mkdir -p /scratch3/scratchdirs/$USER/nostriped
 lfs setstripe -c 1 /scratch3/scratchdirs/$USER/nostriped
-
-srun -n 1536 -N 96 $REPO_BASE/ior/install/bin/ior -s 4096 -H -o /scratch1/scratchdirs/$USER/striped/ior.out -f mpiio1m2.in
-srun -n 1536 -N 96 $REPO_BASE/ior/install/bin/ior -s 4096 -H -o /scratch2/scratchdirs/$USER/striped/ior.out -f mpiio1m2.in
-srun -n 1536 -N 96 $REPO_BASE/ior/install/bin/ior -s 4096 -H -o /scratch3/scratchdirs/$USER/striped/ior.out -f mpiio1m2.in
-srun -n 1536 -N 96 $REPO_BASE/ior/install/bin/ior -s 4096 -H -o /scratch1/scratchdirs/$USER/nostriped/ior.out -f posix1m2.in
-srun -n 1536 -N 96 $REPO_BASE/ior/install/bin/ior -s 4096 -H -o /scratch2/scratchdirs/$USER/nostriped/ior.out -f posix1m2.in
-srun -n 1536 -N 96 $REPO_BASE/ior/install/bin/ior -s 4096 -H -o /scratch3/scratchdirs/$USER/nostriped/ior.out -f posix1m2.in
+srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch1/scratchdirs/$USER/striped/ior.out -f ${REPO_BASE}/inputs/mpiio1m2.in
+srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch2/scratchdirs/$USER/striped/ior.out -f ${REPO_BASE}/inputs/mpiio1m2.in
+srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch3/scratchdirs/$USER/striped/ior.out -f ${REPO_BASE}/inputs/mpiio1m2.in
+srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch1/scratchdirs/$USER/nostriped/ior.out -f ${REPO_BASE}/inputs/posix1m2.in
+srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch2/scratchdirs/$USER/nostriped/ior.out -f ${REPO_BASE}/inputs/posix1m2.in
+srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch3/scratchdirs/$USER/nostriped/ior.out -f ${REPO_BASE}/inputs/posix1m2.in
 rm -rf /scratch1/scratchdirs/$USER/striped/ior.out*
 rm -rf /scratch2/scratchdirs/$USER/striped/ior.out*
 rm -rf /scratch3/scratchdirs/$USER/striped/ior.out*
 rm -rf /scratch1/scratchdirs/$USER/nostriped/ior.out*
 rm -rf /scratch2/scratchdirs/$USER/nostriped/ior.out*
 rm -rf /scratch3/scratchdirs/$USER/nostriped/ior.out*
-
-srun -n 1536 -N 96 $REPO_BASE/hacc-io/install/hacc_io_write 20971520 /scratch1/scratchdirs/$USER/nostriped/haccio-write.out
-srun -n 1536 -N 96 $REPO_BASE/hacc-io/install/hacc_io_write 20971520 /scratch2/scratchdirs/$USER/nostriped/haccio-write.out
-srun -n 1536 -N 96 $REPO_BASE/hacc-io/install/hacc_io_write 20971520 /scratch3/scratchdirs/$USER/nostriped/haccio-write.out
-srun -n 1536 -N 96 $REPO_BASE/hacc-io/install/hacc_io_read 20971520 /scratch1/scratchdirs/$USER/nostriped/haccio-read.out
-srun -n 1536 -N 96 $REPO_BASE/hacc-io/install/hacc_io_read 20971520 /scratch2/scratchdirs/$USER/nostriped/haccio-read.out
-srun -n 1536 -N 96 $REPO_BASE/hacc-io/install/hacc_io_read 20971520 /scratch3/scratchdirs/$USER/nostriped/haccio-read.out
-rm -rf /scratch1/scratchdirs/$USER/nostriped/haccio-write.out*
-rm -rf /scratch2/scratchdirs/$USER/nostriped/haccio-write.out*
-rm -rf /scratch3/scratchdirs/$USER/nostriped/haccio-write.out*
-rm -rf /scratch1/scratchdirs/$USER/nostriped/haccio-read.out*
-rm -rf /scratch2/scratchdirs/$USER/nostriped/haccio-read.out*
-rm -rf /scratch3/scratchdirs/$USER/nostriped/haccio-read.out*
-
-srun -n 1536 -N 96 $REPO_BASE/vpic-io/install/vpicio_uni_dyn /scratch1/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 $REPO_BASE/vpic-io/install/vpicio_uni_dyn /scratch2/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 $REPO_BASE/vpic-io/install/vpicio_uni_dyn /scratch3/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 $REPO_BASE/vpic-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch1/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 $REPO_BASE/vpic-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch2/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 $REPO_BASE/vpic-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch3/scratchdirs/$USER/striped/vpicio.hdf5
+srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_write 20971520 /scratch1/scratchdirs/$USER/nostriped/haccio.out
+srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_write 20971520 /scratch2/scratchdirs/$USER/nostriped/haccio.out
+srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_write 20971520 /scratch3/scratchdirs/$USER/nostriped/haccio.out
+srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_read 20971520 /scratch1/scratchdirs/$USER/nostriped/haccio.out
+srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_read 20971520 /scratch2/scratchdirs/$USER/nostriped/haccio.out
+srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_read 20971520 /scratch3/scratchdirs/$USER/nostriped/haccio.out
+rm -rf /scratch1/scratchdirs/$USER/nostriped/haccio.out*
+rm -rf /scratch2/scratchdirs/$USER/nostriped/haccio.out*
+rm -rf /scratch3/scratchdirs/$USER/nostriped/haccio.out*
+rm -rf /scratch1/scratchdirs/$USER/nostriped/haccio.out*
+rm -rf /scratch2/scratchdirs/$USER/nostriped/haccio.out*
+rm -rf /scratch3/scratchdirs/$USER/nostriped/haccio.out*
+srun -n 1536 -N 96 ${REPO_BASE}/vpic-io/install/vpicio_uni_dyn /scratch1/scratchdirs/$USER/striped/vpicio.hdf5
+srun -n 1536 -N 96 ${REPO_BASE}/vpic-io/install/vpicio_uni_dyn /scratch2/scratchdirs/$USER/striped/vpicio.hdf5
+srun -n 1536 -N 96 ${REPO_BASE}/vpic-io/install/vpicio_uni_dyn /scratch3/scratchdirs/$USER/striped/vpicio.hdf5
+srun -n 1536 -N 96 ${REPO_BASE}/dbcats-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch1/scratchdirs/$USER/striped/vpicio.hdf5
+srun -n 1536 -N 96 ${REPO_BASE}/dbcats-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch2/scratchdirs/$USER/striped/vpicio.hdf5
+srun -n 1536 -N 96 ${REPO_BASE}/dbcats-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch3/scratchdirs/$USER/striped/vpicio.hdf5
 rm -rf /scratch1/scratchdirs/$USER/striped/vpicio.hdf5*
 rm -rf /scratch2/scratchdirs/$USER/striped/vpicio.hdf5*
 rm -rf /scratch3/scratchdirs/$USER/striped/vpicio.hdf5*
