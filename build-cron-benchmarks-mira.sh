4ddbb2f25260e6af2371225b622623b6d4e8bf29
Shane Snyder <ssnyder@mcs.anl.gov>
2017-01-24 20:07:55 +0000

    update mira build/run scripts

--- build-cron-benchmarks-mira.sh
+++ build-cron-benchmarks-mira.sh
@@ -2,11 +2,38 @@
 
 basedir=$(readlink -f $(dirname "$0"))
 
-# build IOR
-cd $basedir/ior
-mkdir -p build
-./bootstrap
-cd build
-../configure --prefix=$basedir/ior/install --host=powerpc-bgq-linux
-make install
+if [ "x$1" == "x" ]; then
+    target="all"
+else
+    target="$1"
+fi
 
+if [ "$target" == "ior" -o "$target" == "all" ]; then
+    # build IOR
+    echo ""
+    echo "*********************"
+    echo "***** IOR BUILD *****"
+    echo "*********************"
+    echo ""
+
+    cd $basedir/ior
+    mkdir -p build
+    ./bootstrap
+    cd build
+    ../configure --prefix=$basedir/ior/install --host=powerpc-bgq-linux
+    make install
+fi
+
+if [ "$target" == "hacc-io" -o "$target" == "all" ]; then
+    # build HACC-IO
+    echo ""
+    echo "*************************"
+    echo "***** HACC-IO BUILD *****"
+    echo "*************************"
+    echo ""
+
+    cd $basedir/hacc-io
+    make CXX=mpicxx fpp
+    mkdir -p install/bin
+    mv -v hacc_io hacc_io_write hacc_io_read hacc_open_close install/bin
+fi
--- run-cron-benchmarks-mira.sh
+++ run-cron-benchmarks-mira.sh
@@ -3,10 +3,59 @@
 basedir=$(readlink -f $(dirname "$0"))
 
 ior_exe=${PROJ_WD:-$basedir/ior/install}/bin/ior
-ior_outdir=${PROJ_WD}/tmp
+ior_outdir=${PROJ_WD:-/projects/radix-io/snyder}/tmp
 
-# run ior shared file configuration
-runjob -n 8192 -p 16 --block $COBALT_PARTNAME --verbose=INFO : $ior_exe -a MPIIO -w -r -c -b 4M -t 4M -s 8 -v -o $ior_outdir/ior.dat
+function printl() {
+    echo "[$(date)] $@"
+}
 
-# TODO run simple hacc-io, ls, etc tests
+read -r -d '' IOR_PARAM_SETS <<EOF
+# ior_api output_file               nproc
+# ------- ------------------------- -----
+  mpiio   $ior_outdir/ior-mpiio.out 256
+EOF
+#  posix   $ior_outdir/ior-posix.out 256
 
+################################################################################
+###  Helper functions to read and execute system-specific parameter sets
+################################################################################
+
+function run_ior() {
+    IOR_API="$1"
+    OUT_FILE="$2"
+    NPROCS="$3"
+
+    printl "Submitting IOR: $IOR_API"
+    runjob -n $NPROCS -p 16 --block $COBALT_PARTNAME --verbose=INFO : \
+        $ior_exe -s 128 \
+                 -H \
+                 -o "$OUT_FILE" \
+                 -f $basedir/inputs/"${IOR_API}1m2.in"
+    printl "Completed IOR: $IOR_API"
+}
+
+function clean_ior() {
+    OUT_FILE="$2"
+    printl "Deleting ${OUT_FILE}*"
+    rm -rf ${OUT_FILE}*
+}
+
+################################################################################
+###  IOR - MPI-IO shared-file and POSIX file-per-process
+################################################################################
+echo "$IOR_PARAM_SETS" | while read parameters
+do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]
+    then
+        continue
+    fi
+    run_ior $parameters
+done
+echo "$IOR_PARAM_SETS" | while read parameters
+do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]
+    then
+        continue
+    fi
+    clean_ior $parameters
+done
