1afb3f571c324692f9ba8cc251abc6d340dc1222
Glenn K. Lockwood <glock@lbl.gov>
2017-01-24 21:32:11 -0800

    testing machine-specific wrappers to get around the incompatible sbatch lines required by each system

---
+++ run-wrapper-cori-knl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This submit script is intended to be submitted from the repository's base dir.
+#
+#SBATCH -J tokio-abc
+#SBATCH -N 16
+#SBATCH -p debug
+#SBATCH -t 00:30:00
+#SBATCH -C knl
+#DW jobdw type=scratch access_mode=striped capacity=19200GiB
+#DW jobdw type=scratch access_mode=private capacity=19200GiB
+
+export REPO_BASE_DIR="${SLURM_SUBMIT_DIR}"
+export TOKIO_LOGPATH="${SLURM_SUBMIT_DIR}/runs"
+export DARSHAN_LOGPATH="${TOKIO_LOGPATH}"
+export NERSC_HOST="${NERSC_HOST}-mini"
+
+mkdir -p runs && cd runs
+sbatch ../inputs/omnibus.sbatch
---
+++ run-wrapper-edison.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# This submit script is intended to be submitted from the repository's base dir.
+#
+#SBATCH -J tokio-abc
+#SBATCH -N 16
+#SBATCH -p debug
+#SBATCH -t 00:30:00
+
+export REPO_BASE_DIR="${SLURM_SUBMIT_DIR}"
+export TOKIO_LOGPATH="${SLURM_SUBMIT_DIR}/runs"
+export DARSHAN_LOGPATH="${TOKIO_LOGPATH}"
+export NERSC_HOST="${NERSC_HOST}-mini"
+
+mkdir -p runs && cd runs
+sbatch ../inputs/omnibus.sbatch
