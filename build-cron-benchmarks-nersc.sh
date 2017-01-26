0767175c3b7ae15d802eef72167247fcd444c57e
Shane Snyder <ssnyder@mcs.anl.gov>
2017-01-26 05:47:03 +0000

    small updates to nersc build script

--- build-cron-benchmarks-nersc.sh
+++ build-cron-benchmarks-nersc.sh
@@ -20,49 +20,79 @@ fi
 
 # build IOR
 if [ "$target" == "ior" -o "$target" == "all" ]; then
+    echo ""
+    echo "*********************"
+    echo "***** IOR BUILD *****"
+    echo "*********************"
+    echo ""
+
     cd $basedir/ior
     mkdir -p build
     ./bootstrap
     cd build
     ../configure --prefix=$basedir/ior/install --without-gpfs $EXTRA_AC_FLAGS
-    make install
+    make install || exit 1
 fi
 
 # build H5Part (required by VPIC-IO and BD-CATS-IO)
 if [ "$target" == "h5part" -o "$target" == "all" ]; then
+    echo ""
+    echo "************************"
+    echo "***** H5PART BUILD *****"
+    echo "************************"
+    echo ""
+
     module load cray-hdf5-parallel ### assume HDF5 1.8 or higher
     cd $basedir/h5part
     mkdir -p build
     ./bootstrap
     cd build
     ../configure --prefix=$basedir/h5part/install --enable-parallel $EXTRA_AC_FLAGS
-    make install
+    make install || exit 1
 fi
 
 # build VPIC-IO
 if [ "$target" == "vpic-io" -o "$target" == "all" ]; then
+    echo ""
+    echo "*************************"
+    echo "***** VPIC-IO BUILD *****"
+    echo "*************************"
+    echo ""
+
     module load cray-hdf5-parallel ### assume HDF5 1.8 or higher
     cd "${basedir}/vpic-io"
     sed -e 's@^H5PART_ROOT *=.*$@H5PART_ROOT='"${basedir}/h5part/install"'@' Makefile.in > Makefile
-    make || exit 1
+    make dynamic || exit 1
     mkdir -p install
     mv -v vpicio_uni vpicio_uni_dyn install/
 fi
 
 # build BDCATS-IO
 if [ "$target" == "bdcats-io" -o "$target" == "all" ]; then
+    echo ""
+    echo "***************************"
+    echo "***** BDCATS-IO BUILD *****"
+    echo "***************************"
+    echo ""
+
     module load cray-hdf5-parallel ### assume HDF5 1.8 or higher
     cd "${basedir}/bdcats-io"
     sed -e 's@^H5PART_ROOT *=.*$@H5PART_ROOT='"${basedir}/h5part/install"'@' Makefile.in > Makefile
-    make || exit 1
+    make dynamic || exit 1
     mkdir -p install
     mv -v dbscan_read install/
 fi
 
 # build HACC-IO
 if [ "$target" == "hacc-io" -o "$target" == "all" ]; then
+    echo ""
+    echo "*************************"
+    echo "***** HACC-IO BUILD *****"
+    echo "*************************"
+    echo ""
+
     cd $basedir/hacc-io
-    make CXX=CC fpp
+    make CXX=CC fpp || exit 1
     mkdir -p install
     mv -v hacc_io hacc_io_write hacc_io_read hacc_open_close install/
 fi
