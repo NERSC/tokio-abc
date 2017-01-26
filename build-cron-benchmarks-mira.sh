d9cb3a2f22e0e5cf48a4e3a00a6b9b31f7605700
Shane Snyder <ssnyder@mcs.anl.gov>
2017-01-26 05:46:17 +0000

    mira build script finally working

--- build-cron-benchmarks-mira.sh
+++ build-cron-benchmarks-mira.sh
@@ -8,8 +8,12 @@ else
     target="$1"
 fi
 
+EXTRA_AC_FLAGS="--host=powerpc-bgq-linux"
+HDF5_ROOT=/soft/libraries/hdf5/current/cnk-xl/current/
+ZLIB_ROOT=/soft/libraries/alcf/current/xl/ZLIB
+
+# build IOR
 if [ "$target" == "ior" -o "$target" == "all" ]; then
-    # build IOR
     echo ""
     echo "*********************"
     echo "***** IOR BUILD *****"
@@ -20,12 +24,64 @@ if [ "$target" == "ior" -o "$target" == "all" ]; then
     mkdir -p build
     ./bootstrap
     cd build
-    ../configure --prefix=$basedir/ior/install --host=powerpc-bgq-linux
-    make install
+    ../configure --prefix=$basedir/ior/install $EXTRA_AC_FLAGS CC=mpixlc
+    make install || exit 1
 fi
 
+# build h5part (required by VPIC-IO and BD-CATS-IO
+if [ "$target" == "h5part" -o "$target" == "all" ]; then
+    echo ""
+    echo "************************"
+    echo "***** H5PART BUILD *****"
+    echo "************************"
+    echo ""
+
+    cd $basedir/h5part
+    mkdir -p build
+    ./bootstrap
+    cd build
+    ../configure --enable-parallel --with-hdf5=$HDF5_ROOT --prefix=$basedir/h5part/install $EXTRA_AC_FLAGS CC=mpixlc
+    make install || exit 1
+fi
+
+# build VPIC-IO
+if [ "$target" == "vpic-io" -o "$target" == "all" ]; then
+    echo ""
+    echo "*************************"
+    echo "***** VPIC-IO BUILD *****"
+    echo "*************************"
+    echo ""
+
+    cd $basedir/vpic-io
+    sed -e 's@^H5PART_ROOT *=.*$@H5PART_ROOT='"${basedir}/h5part/install"'@' Makefile.in > Makefile
+    export VPIC_CFLAGS="-I${HDF5_ROOT}/include -I${ZLIB_ROOT}/include"
+    export VPIC_LDFLAGS="-L${HDF5_ROOT}/lib -L${ZLIB_ROOT}/lib"
+    export VPIC_LDLIBS="-lhdf5 -lz"
+    make CC=mpixlc || exit 1
+    mkdir -p install/bin
+    mv -v vpicio_uni install/bin
+fi
+
+# build BDCATS-IO
+if [ "$target" == "bdcats-io" -o "$target" == "all" ]; then
+    echo ""
+    echo "***************************"
+    echo "***** BDCATS-IO BUILD *****"
+    echo "***************************"
+    echo ""
+
+    cd $basedir/bdcats-io
+    sed -e 's@^H5PART_ROOT *=.*$@H5PART_ROOT='"${basedir}/h5part/install"'@' Makefile.in > Makefile
+    export BDCATS_CFLAGS="-I${HDF5_ROOT}/include -I${ZLIB_ROOT}/include"
+    export BDCATS_LDFLAGS="-L${HDF5_ROOT}/lib -L${ZLIB_ROOT}/lib"
+    export BDCATS_LDLIBS="-lhdf5 -lz"
+    make CC=mpixlcxx || exit 1
+    mkdir -p install/bin
+    mv -v dbscan_read install/bin
+fi
+
+# build HACC-IO
 if [ "$target" == "hacc-io" -o "$target" == "all" ]; then
-    # build HACC-IO
     echo ""
     echo "*************************"
     echo "***** HACC-IO BUILD *****"
@@ -33,7 +89,7 @@ if [ "$target" == "hacc-io" -o "$target" == "all" ]; then
     echo ""
 
     cd $basedir/hacc-io
-    make CXX=mpicxx fpp
+    make CXX=mpixlcxx fpp || exit 1
     mkdir -p install/bin
     mv -v hacc_io hacc_io_write hacc_io_read hacc_open_close install/bin
 fi
