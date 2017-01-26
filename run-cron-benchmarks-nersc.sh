980be5f3c760c9966d3fab8fe87f38d995993aa1
Glenn K. Lockwood <glock@lbl.gov>
2017-01-26 12:56:34 -0800

    inputs/omnibus.sbatch -> run-cron-benchmarks-nersc.sh

--- run-cron-benchmarks-nersc.sh
+++ run-cron-benchmarks-nersc.sh
@@ -1,118 +1,298 @@
 #!/bin/bash
-### To run on NERSC Cori, do something like (for KNL)
-###
-###    sbatch --bb=./bb-multi.conf -C knl omnibus.sbatch
-###
-### or (for Haswell)
-###
-###    sbatch --bb=./bb-multi.conf -C haswell omnibus.sbatch
-###
-### 2304 procs = 96 nodes on Edison, 72 on Cori/Haswell, 36 on Cori/KNL(@64c)
-### 1536 procs = 64 nodes on Edison, 48 on Cori/Haswell, 24 on Cori/KNL(@64c)
-#SBATCH -J tokio-abc
-#SBATCH -N 96
-#SBATCH -p regular
-#SBATCH -t 04:00:00
-#SBATCH --mail-user=glock@lbl.gov --mail-type=END
-#SBATCH -C knl
-#DW jobdw type=scratch access_mode=striped capacity=19200GiB
-#DW jobdw type=scratch access_mode=private capacity=19200GiB
+### This submit script is intended to be run from a subdirectory of the
+### repository root, e.g., /home/$USER/this-repo/runs.  If you want to do
+### something different, export REPO_BASE_DIR before invoking this script.
 
-module load lustre-cray_ari_s
+### implicit that this script is run from a subdirectory of the repo base
+export REPO_BASE_DIR=${REPO_BASE_DIR:-$(readlink -f $PWD/..)}
 
-function run_edison() {
-REPO_BASE="/global/u2/g/glock/src/git/tokio-paper-edison"
-
-mkdir -p /scratch1/scratchdirs/$USER/striped
-lfs setstripe -c -1 /scratch1/scratchdirs/$USER/striped
-mkdir -p /scratch2/scratchdirs/$USER/striped
-lfs setstripe -c -1 /scratch2/scratchdirs/$USER/striped
-mkdir -p /scratch3/scratchdirs/$USER/striped
-lfs setstripe -c -1 /scratch3/scratchdirs/$USER/striped
-mkdir -p /scratch1/scratchdirs/$USER/nostriped
-lfs setstripe -c 1 /scratch1/scratchdirs/$USER/nostriped
-mkdir -p /scratch2/scratchdirs/$USER/nostriped
-lfs setstripe -c 1 /scratch2/scratchdirs/$USER/nostriped
-mkdir -p /scratch3/scratchdirs/$USER/nostriped
-lfs setstripe -c 1 /scratch3/scratchdirs/$USER/nostriped
-srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch1/scratchdirs/$USER/striped/ior.out -f ${REPO_BASE}/inputs/mpiio1m2.in
-srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch2/scratchdirs/$USER/striped/ior.out -f ${REPO_BASE}/inputs/mpiio1m2.in
-srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch3/scratchdirs/$USER/striped/ior.out -f ${REPO_BASE}/inputs/mpiio1m2.in
-srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch1/scratchdirs/$USER/nostriped/ior.out -f ${REPO_BASE}/inputs/posix1m2.in
-srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch2/scratchdirs/$USER/nostriped/ior.out -f ${REPO_BASE}/inputs/posix1m2.in
-srun -n 1536 -N 96 ${REPO_BASE}/ior/install/bin/ior -s 4096 -H -o /scratch3/scratchdirs/$USER/nostriped/ior.out -f ${REPO_BASE}/inputs/posix1m2.in
-rm -rf /scratch1/scratchdirs/$USER/striped/ior.out*
-rm -rf /scratch2/scratchdirs/$USER/striped/ior.out*
-rm -rf /scratch3/scratchdirs/$USER/striped/ior.out*
-rm -rf /scratch1/scratchdirs/$USER/nostriped/ior.out*
-rm -rf /scratch2/scratchdirs/$USER/nostriped/ior.out*
-rm -rf /scratch3/scratchdirs/$USER/nostriped/ior.out*
-srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_write 20971520 /scratch1/scratchdirs/$USER/nostriped/haccio.out
-srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_write 20971520 /scratch2/scratchdirs/$USER/nostriped/haccio.out
-srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_write 20971520 /scratch3/scratchdirs/$USER/nostriped/haccio.out
-srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_read 20971520 /scratch1/scratchdirs/$USER/nostriped/haccio.out
-srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_read 20971520 /scratch2/scratchdirs/$USER/nostriped/haccio.out
-srun -n 1536 -N 96 ${REPO_BASE}/hacc-io/install/hacc_io_read 20971520 /scratch3/scratchdirs/$USER/nostriped/haccio.out
-rm -rf /scratch1/scratchdirs/$USER/nostriped/haccio.out*
-rm -rf /scratch2/scratchdirs/$USER/nostriped/haccio.out*
-rm -rf /scratch3/scratchdirs/$USER/nostriped/haccio.out*
-rm -rf /scratch1/scratchdirs/$USER/nostriped/haccio.out*
-rm -rf /scratch2/scratchdirs/$USER/nostriped/haccio.out*
-rm -rf /scratch3/scratchdirs/$USER/nostriped/haccio.out*
-srun -n 1536 -N 96 ${REPO_BASE}/vpic-io/install/vpicio_uni_dyn /scratch1/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 ${REPO_BASE}/vpic-io/install/vpicio_uni_dyn /scratch2/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 ${REPO_BASE}/vpic-io/install/vpicio_uni_dyn /scratch3/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 ${REPO_BASE}/dbcats-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch1/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 ${REPO_BASE}/dbcats-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch2/scratchdirs/$USER/striped/vpicio.hdf5
-srun -n 1536 -N 96 ${REPO_BASE}/dbcats-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /scratch3/scratchdirs/$USER/striped/vpicio.hdf5
-rm -rf /scratch1/scratchdirs/$USER/striped/vpicio.hdf5*
-rm -rf /scratch2/scratchdirs/$USER/striped/vpicio.hdf5*
-rm -rf /scratch3/scratchdirs/$USER/striped/vpicio.hdf5*
-rm -rf /scratch1/scratchdirs/$USER/striped/vpicio.hdf5*
-rm -rf /scratch2/scratchdirs/$USER/striped/vpicio.hdf5*
-rm -rf /scratch3/scratchdirs/$USER/striped/vpicio.hdf5*
-}
-
-function run_cori_knl() {
-mkdir -p /global/cscratch1/sd/$USER/striped
-lfs setstripe -c -1 /global/cscratch1/sd/$USER/striped
-mkdir -p /global/cscratch1/sd/$USER/nostriped
-lfs setstripe -c 1 /global/cscratch1/sd/$USER/nostriped
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../ior/install/bin/ior -s 4096 -H -o /global/cscratch1/sd/$USER/striped/ior.out -f $SLURM_SUBMIT_DIR/../inputs/mpiio1m2.in > "ior-1.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../ior/install/bin/ior -s 4096 -H -o /global/cscratch1/sd/$USER/nostriped/ior.out -f $SLURM_SUBMIT_DIR/../inputs/posix1m2.in > "ior-2.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../ior/install/bin/ior -s 4096 -H -o $DW_JOB_STRIPED/ior.out -f $SLURM_SUBMIT_DIR/../inputs/mpiio1m2.in > "ior-3.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../ior/install/bin/ior -s 4096 -H -o $DW_JOB_PRIVATE/ior.out -f $SLURM_SUBMIT_DIR/../inputs/posix1m2.in > "ior-4.${SLURM_JOBID}.out" 2>&1
-rm -rf /global/cscratch1/sd/$USER/striped/ior.out*
-rm -rf /global/cscratch1/sd/$USER/nostriped/ior.out*
-rm -rf $DW_JOB_STRIPED/ior.out*
-rm -rf $DW_JOB_PRIVATE/ior.out*
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../hacc-io/install/hacc_io_write 20971520 /global/cscratch1/sd/$USER/nostriped/haccio.out > "haccio-write-1.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../hacc-io/install/hacc_io_write 20971520 $DW_JOB_STRIPED/haccio.out > "haccio-write-2.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../hacc-io/install/hacc_io_write 20971520 $DW_JOB_PRIVATE/haccio.out > "haccio-write-3.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../hacc-io/install/hacc_io_read 20971520 /global/cscratch1/sd/$USER/nostriped/haccio.out > "haccio-read-1.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../hacc-io/install/hacc_io_read 20971520 $DW_JOB_STRIPED/haccio.out > "haccio-read-2.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../hacc-io/install/hacc_io_read 20971520 $DW_JOB_PRIVATE/haccio.out > "haccio-read-4.${SLURM_JOBID}.out" 2>&1
-rm -rf /global/cscratch1/sd/$USER/nostriped/haccio.out*
-rm -rf $DW_JOB_STRIPED/haccio.out*
-rm -rf $DW_JOB_PRIVATE/haccio.out*
-rm -rf /global/cscratch1/sd/$USER/nostriped/haccio.out*
-rm -rf $DW_JOB_STRIPED/haccio.out*
-rm -rf $DW_JOB_PRIVATE/haccio.out*
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../vpic-io/install/vpicio_uni_dyn /global/cscratch1/sd/$USER/striped/vpicio.hdf5 > "vpicio-1.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../vpic-io/install/vpicio_uni_dyn $DW_JOB_STRIPED/vpicio.hdf5 > "vpicio-2.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../bdcats-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f /global/cscratch1/sd/$USER/striped/vpicio.hdf5 > "bdcatsio-1.${SLURM_JOBID}.out" 2>&1
-srun -n 1536 -N 96 $SLURM_SUBMIT_DIR/../bdcats-io/install/dbscan_read -d '/Step#0/x' -d '/Step#0/y' -d '/Step#0/z' -d '/Step#0/px' -d '/Step#0/py' -d '/Step#0/pz' -f $DW_JOB_STRIPED/vpicio.hdf5 > "bdcatsio-1.${SLURM_JOBID}.out" 2>&1
-rm -rf /global/cscratch1/sd/$USER/striped/vpicio.hdf5*
-rm -rf $DW_JOB_STRIPED/vpicio.hdf5*
-rm -rf /global/cscratch1/sd/$USER/striped/vpicio.hdf5*
-rm -rf $DW_JOB_STRIPED/vpicio.hdf5*
-}
-
-if [ "$NERSC_HOST" == "edison" ]; then
-    run_edison
-elif [ "$NERSC_HOST" == "cori" ]; then
-    run_cori_knl
+### if not running in Slurm, just do a dry run
+if [ -z "$SLURM_JOBID" ]; then
+    srun_exe="echo srun"
+    rm_exe="echo rm"
+    mkdir_exe="echo mkdir"
+    lfs_exe="echo lfs"
+    export TOKIO_JOBID="0000000"
+    export TOKIO_JOBDIR="$PWD"
+    export DW_JOB_STRIPED='$DW_JOB_STRIPED' # export so that `envsubst` sees it
+    export DW_JOB_PRIVATE='$DW_JOB_PRIVATE' # export so that `envsubst` sees it
+    function printlog() {
+        :
+    }
+    function printerr() {
+        echo "[$(date)] $@" >&2
+    }
 else
-    echo "Unknown NERSC_HOST(=$NERSC_HOST)" >&2
+    srun_exe="srun"
+    rm_exe="rm"
+    mkdir_exe="mkdir"
+    lfs_exe="lfs"
+    export TOKIO_JOBID="$SLURM_JOBID"
+    export TOKIO_JOBDIR="$SLURM_SUBMIT_DIR"
+    function printlog() {
+        echo "[$(date)] $@"
+    }
+    function printerr() {
+        echo "[$(date)] $@" >&2
+    }
+fi
+
+### location to dump the stderr/stdout of each benchmark
+export TOKIO_LOGPATH=${TOKIO_LOGPATH:-$TOKIO_JOBDIR}
+
+### the following assumes Darshan was configured with
+###    --with-log-path-by-env=DARSHAN_LOGPATH
+export DARSHAN_LOGPATH="$TOKIO_LOGPATH"
+printlog "Darshan logs will go to $DARSHAN_LOGPATH"
+
+### Enable extra verbosity in MPI-IO to get insight into collective buffering
+export MPICH_MPIIO_HINTS_DISPLAY=1
+export MPICH_MPIIO_STATS=1
+
+### these paths should not require site-specific modification
+ior_exe="${REPO_BASE_DIR}/ior/install/bin/ior"
+hacc_exe_path="${REPO_BASE_DIR}/hacc-io/install"
+vpic_exe_path="${REPO_BASE_DIR}"
+
+################################################################################
+###  System-specific input and setup parameters
+################################################################################
+
+###  Edison  ###################################################################
+
+if [ "$NERSC_HOST" == "edison" -o "$NERSC_HOST" == "edison-mini" -o "$NERSC_HOST" == "edison-micro" ]; then
+    ### Set up a directory with wide striping
+    module load lustre-cray_ari_s
+    for out_dir in "/scratch1/scratchdirs/$USER/striped" \
+                   "/scratch2/scratchdirs/$USER/striped" \
+                   "/scratch3/scratchdirs/$USER/striped"
+    do
+        $mkdir_exe -p $out_dir && $lfs_exe setstripe -c -1 $out_dir
+    done
+    ### Set up a directory with no striping (for file-per-process)
+    for out_dir in "/scratch1/scratchdirs/$USER/nostriped" \
+                   "/scratch2/scratchdirs/$USER/nostriped" \
+                   "/scratch3/scratchdirs/$USER/nostriped"
+    do
+        $mkdir_exe -p $out_dir && $lfs_exe setstripe -c 1 $out_dir
+    done
+
+    IOR_PARAMS_FILE="${REPO_BASE_DIR}/inputs/ior-${NERSC_HOST}.params"
+    HACCIO_PARAMS_FILE="${REPO_BASE_DIR}/inputs/haccio-${NERSC_HOST}.params"
+    VPICIO_PARAMS_FILE="${REPO_BASE_DIR}/inputs/vpicio-${NERSC_HOST}.params"
+
+###  Cori  #####################################################################
+elif [ "$NERSC_HOST" == "cori" -o "$NERSC_HOST" == "cori-mini" -o "$NERSC_HOST" == "cori-micro" ]; then
+    ### Set up a directory with wide striping
+    module load lustre-cray_ari_s
+    $mkdir_exe -p $SCRATCH/striped && $lfs_exe setstripe -c -1 $SCRATCH/striped
+
+    ### ...and with no striping
+    $mkdir_exe -p $SCRATCH/nostriped && $lfs_exe setstripe -c 1 $SCRATCH/nostriped
+
+    IOR_PARAMS_FILE="${REPO_BASE_DIR}/inputs/ior-${NERSC_HOST}.params"
+    HACCIO_PARAMS_FILE="${REPO_BASE_DIR}/inputs/haccio-${NERSC_HOST}.params"
+    VPICIO_PARAMS_FILE="${REPO_BASE_DIR}/inputs/vpicio-${NERSC_HOST}.params"
+
+###  Undefined #################################################################
+elif [ -z "$NERSC_HOST" ]; then
+    printerr "Undefined NERSC_HOST" >&2
+    exit 1
+
+###  Unknown  ##################################################################
+else
+    printerr "Unknown NERSC_HOST [$NERSC_HOST]" >&2
     exit 1
 fi
+
+################################################################################
+###  Helper functions to read and execute system-specific parameter sets
+################################################################################
+function run_ior() {
+    FS_NAME="$1"
+    IOR_API="$2"
+    READ_OR_WRITE="$3"
+    OUT_FILE="$4"
+    NNODES="$5"
+    NPROCS="$6"
+
+    if [ "$READ_OR_WRITE" == "write" ]; then
+        IOR_CLI_ARGS="-k -w"
+    elif [ "$READ_OR_WRITE" == "read" ]; then
+        IOR_CLI_ARGS="-r"
+    else
+        printerr "Unknown read-or-write parameter [$READ_OR_WRITE]"
+        IOR_CLI_ARGS=""
+        # warn, but attempt to run r+w
+    fi
+
+    mkdir -p "$(dirname "$OUT_FILE")"
+    printlog "Submitting IOR: ${FS_NAME}-${IOR_API}"
+    MPICH_MPIIO_HINTS="*:romio_cb_read=enable:romio_cb_write=enable"
+    # write-only, keep output files
+    MPICH_MPIIO_HINTS="$MPICH_MPIIO_HINTS" \
+        $srun_exe -n ${NPROCS} \
+         -N ${NNODES} \
+         "$ior_exe" -H \
+                    $IOR_CLI_ARGS \
+                    -o "${OUT_FILE}" \
+                    -f "${REPO_BASE_DIR}/inputs/${IOR_API}1m2.in" | tee "${TOKIO_LOGPATH}/ior_${READ_OR_WRITE}-${FS_NAME}-${IOR_API}.${TOKIO_JOBID}.out"
+    printlog "Completed IOR: ${FS_NAME}-${IOR_API}"
+}
+
+function clean_ior() {
+    OUT_FILE="$3"
+    if [ ! -z "$OUT_FILE" ]; then
+        printlog "Deleting ${OUT_FILE}*"
+        ### if this is a private DataWarp namespace, we must delete all matches
+        ### from all namespaces
+        if [[ ! -z "$DW_JOB_PRIVATE" && "$OUT_FILE" =~ $DW_JOB_PRIVATE ]]; then
+            $srun_exe --ntasks-per-node=1 bash -c "rm -f ${OUT_FILE}*"
+        else
+            $rm_exe -rf ${OUT_FILE}*
+        fi
+    fi
+}
+
+function run_haccio() {
+    FS_NAME="$1"
+    HACC_EXE="$2"
+    OUT_FILE="$3"
+    NNODES="$4"
+    NPROCS="$5"
+
+    mkdir -p "$(dirname "$OUT_FILE")"
+    printlog "Submitting HACC-IO: ${FS_NAME}-${HACC_EXE}"
+    $srun_exe -n ${NPROCS} -N ${NNODES} "${hacc_exe_path}/${HACC_EXE}" 28256364 "${OUT_FILE}" | tee "${TOKIO_LOGPATH}/haccio-${FS_NAME}-${HACC_EXE}.${TOKIO_JOBID}.out"
+    printlog "Completed HACC-IO: ${FS_NAME}-${HACC_EXE}"
+}
+
+function clean_haccio() {
+    OUT_FILE="$3"
+    if [ ! -z "$OUT_FILE" ]; then
+        printlog "Deleting ${OUT_FILE}*"
+        ### if this is a private DataWarp namespace, we must delete all matches from
+        ### all namespaces
+        if [[ ! -z "$DW_JOB_PRIVATE" && "$OUT_FILE" =~ $DW_JOB_PRIVATE ]]; then
+            $srun_exe --ntasks-per-node=1 bash -c "rm -f ${OUT_FILE}*"
+        else
+            $rm_exe -rf ${OUT_FILE}*
+        fi
+    fi
+}
+
+function run_vpicio() {
+    FS_NAME="$1"
+    VPIC_EXE="$2"
+    OUT_FILE="$3"
+    NNODES="$4"
+    NPROCS="$5"
+
+    mkdir -p "$(dirname "$OUT_FILE")"
+    if [[ "$VPIC_EXE" =~ dbscan_read.* ]]; then
+        extra_args="-d /Step#0/x -d /Step#0/y -d /Step#0/z -d /Step#0/px -d /Step#0/py -d /Step#0/pz -f"
+    elif [[ "$VPIC_EXE" =~ vpicio_uni.* ]]; then
+        extra_args=""
+    else
+        printerr "Unknown VPIC exe [$VPIC_EXE]; not passing any extra CLI args" >&2
+        extra_args=""
+    fi
+    printlog "Submitting VPIC-IO: ${FS_NAME}-$(basename ${VPIC_EXE})"
+    $srun_exe -n ${NPROCS} -N ${NNODES} "${vpic_exe_path}/${VPIC_EXE}" $extra_args "${OUT_FILE}" | tee "${TOKIO_LOGPATH}/vpicio-${FS_NAME}-$(basename ${VPIC_EXE}).${TOKIO_JOBID}.out"
+    printlog "Completed VPIC-IO: ${FS_NAME}-$(basename ${VPIC_EXE})"
+}
+
+function clean_vpicio() {
+    OUT_FILE="$3"
+    if [ ! -z "$OUT_FILE" ]; then
+        printlog "Deleting ${OUT_FILE}*"
+        ### if this is a private DataWarp namespace, we must delete all matches from
+        ### all namespaces
+        if [[ ! -z "$DW_JOB_PRIVATE" && "$OUT_FILE" =~ $DW_JOB_PRIVATE ]]; then
+            $srun_exe --ntasks-per-node=1 bash -c "rm -f ${OUT_FILE}*"
+        else
+            $rm_exe -rf ${OUT_FILE}*
+        fi
+    fi
+}
+
+################################################################################
+###  IOR - MPI-IO shared-file and POSIX file-per-process
+################################################################################
+if [ ! -f "$IOR_PARAMS_FILE" ]; then
+    printerr "IOR_PARAMS_FILE=[$IOR_PARAMS_FILE] not found"
+    IOR_PARAMS_FILE=/dev/null
+fi
+PARAM_LINES=()
+while read -r parameters; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    PARAM_LINES+=("$parameters")
+done <<< "$(envsubst < "$IOR_PARAMS_FILE")"
+for parameters in "${PARAM_LINES[@]}"; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    run_ior $parameters
+done
+for parameters in "${PARAM_LINES[@]}"; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    clean_ior $parameters
+done
+
+################################################################################
+###  HACC-IO - Write and read using GLEAN file-per-process
+################################################################################
+if [ ! -f "$HACCIO_PARAMS_FILE" ]; then
+    printerr "HACCIO_PARAMS_FILE=[$HACCIO_PARAMS_FILE] not found"
+    HACCIO_PARAMS_FILE=/dev/null
+fi
+PARAM_LINES=()
+while read -r parameters; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    PARAM_LINES+=("$parameters")
+done <<< "$(envsubst < "$HACCIO_PARAMS_FILE")"
+for parameters in "${PARAM_LINES[@]}"; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    run_haccio $parameters
+done
+for parameters in "${PARAM_LINES[@]}"; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    clean_haccio $parameters
+done
+
+################################################################################
+###  VPIC-IO - Write and read using HDF5 shared file (VPIC-IO and BD-CATS-IO)
+################################################################################
+if [ ! -f "$VPICIO_PARAMS_FILE" ]; then
+    printerr "VPICIO_PARAMS_FILE=[$VPICIO_PARAMS_FILE] not found"
+    VPICIO_PARAMS_FILE=/dev/null
+fi
+PARAM_LINES=()
+while read -r parameters; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    PARAM_LINES+=("$parameters")
+done <<< "$(envsubst < "$VPICIO_PARAMS_FILE")"
+for parameters in "${PARAM_LINES[@]}"; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    run_vpicio $parameters
+done
+for parameters in "${PARAM_LINES[@]}"; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    clean_vpicio $parameters
+done
--- run-wrapper-edison.sh
+++ run-wrapper-edison.sh
@@ -3,14 +3,14 @@
 # This submit script is intended to be submitted from the repository's base dir.
 #
 #SBATCH -J tokio-abc
-#SBATCH -N 4
-#SBATCH -p debug
-#SBATCH -t 00:30:00
+#SBATCH -N 96
+#SBATCH -p regular
+#SBATCH -t 02:00:00
 
 export REPO_BASE_DIR="${SLURM_SUBMIT_DIR}"
 export TOKIO_LOGPATH="${SLURM_SUBMIT_DIR}/runs"
 export DARSHAN_LOGPATH="${TOKIO_LOGPATH}"
-export NERSC_HOST="${NERSC_HOST}-micro"
+export NERSC_HOST="${NERSC_HOST}"
 
 mkdir -p runs && cd runs
-../inputs/omnibus.sbatch
+../run-cron-benchmarks-nersc.sh
