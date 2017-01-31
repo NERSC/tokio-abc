0e682f35788f23f587982b57bed9c021ca579b1b
Glenn K. Lockwood <glock@lbl.gov>
2017-01-31 13:05:10 -0800

    run wrappers shouldn't clobber each other

--- run-wrapper-cori-haswell.sh
+++ run-wrapper-cori-haswell.sh
@@ -2,18 +2,23 @@
 #
 # This submit script is intended to be submitted from the repository's base dir.
 #
-#SBATCH -J tokio-abc
+#SBATCH -J tokio-abc-cori-haswell
 #SBATCH -N 96
 #SBATCH -p regular
 #SBATCH -t 02:00:00
 #SBATCH -C haswell
 #DW jobdw type=scratch access_mode=striped capacity=7TiB pool=sm_pool
 #DW jobdw type=scratch access_mode=private capacity=7TiB pool=sm_pool
+i=0
+while [ -d runs.$i ]; do
+    let "i++"
+done
+echo "[$(date)] Outputting to runs.$i"
 
 export REPO_BASE_DIR="${SLURM_SUBMIT_DIR}"
-export TOKIO_LOGPATH="${SLURM_SUBMIT_DIR}/runs"
+export TOKIO_LOGPATH="${SLURM_SUBMIT_DIR}/runs.$i"
 export DARSHAN_LOGPATH="${TOKIO_LOGPATH}"
 export NERSC_HOST="${NERSC_HOST}"
 
-mkdir -p runs && cd runs
+mkdir -p "$TOKIO_LOGPATH" && cd "$TOKIO_LOGPATH"
 ../run-cron-benchmarks-nersc.sh
