760fbba6ad0ee122b7eb0b4d15ddef212d34d524
Shane Snyder <ssnyder@mcs.anl.gov>
2017-01-31 22:07:08 +0000

    update mira run benchmarks script

--- run-cron-benchmarks-mira.sh
+++ run-cron-benchmarks-mira.sh
@@ -1,44 +1,67 @@
 #!/bin/bash -x
 #COBALT -A radix-io
-#COBALT -n 1024 
+#COBALT -n 1024
 #COBALT -t 30
-#OCBALT --mode script
+#COBALT --mode script
 
-basedir=$(readlink -f $(dirname "$0"))
+TOKIO_JOB_DIR=${TOKIO_JOB_DIR:-$(readlink -f $PWD/..)}
+TOKIO_OUT_DIR=${TOKIO_OUT_DIR:-$TOKIO_JOB_DIR/tmp}
 
-ior_exe=${PROJ_WD:-$basedir/ior/install}/bin/ior
-ior_outdir=${PROJ_WD:-/projects/radix-io/snyder}/tmp
-inputdir=${PROJ_WD:-$basedir}/inputs
 error_code=0
 
-function printl() {
+function printlog() {
     echo "[$(date)] $@"
 }
+function printerr() {
+    echo "[$(date)] $@" >&2
+}
 
-read -r -d '' IOR_PARAM_SETS <<EOF
-# ior_api output_file               nproc
-# ------- ------------------------- -----
-  mpiio   $ior_outdir/ior-mpiio.out 16384
-EOF
-#  posix   $ior_outdir/ior-posix.out 256
+export MPICH_MPIIO_HINTS="*:romio_cb_read=enable:romio_cb_write=enable"
 
 ################################################################################
 ###  Helper functions to read and execute system-specific parameter sets
 ################################################################################
 
+IOR_EXE=${TOKIO_JOB_DIR}/bin/ior
+
 function run_ior() {
     IOR_API="$1"
-    OUT_FILE="$2"
-    NPROCS="$3"
+    READ_OR_WRITE="$2"
+    OUT_FILE="$3"
+    NPROCS="$4"
 
-    printl "Submitting IOR: $IOR_API"
+    if [ "$READ_OR_WRITE" == "write" ]; then
+        IOR_CLI_ARGS="-k -w"
+    elif [ "$READ_OR_WRITE" == "read" ]; then
+        IOR_CLI_ARGS="-r"
+    else
+        printerr "Unknown read-or-write parameter [$READ_OR_WRITE]"
+        IOR_CLI_ARGS=""
+        # warn, but attempt to run r+w
+    fi
+
+    if [ "$IOR_API" == "mpiio" ]; then
+        ### Enable extra verbosity in MPI-IO to get insight into collective buffering
+        export MPICH_MPIIO_HINTS_DISPLAY=1
+        export MPICH_MPIIO_STATS=1
+    fi
+
+    printlog "Submitting IOR: $IOR_API-$READ_OR_WRITE"
     runjob -n $NPROCS -p 16 --block $COBALT_PARTNAME --envs BGLOCKLESSMPIO_F_TYPE=0x47504653 --verbose=INFO : \
-        $ior_exe -s 128 \
-                 -H \
-                 -o "$OUT_FILE" \
-                 -f ${inputdir}/"${IOR_API}1m2.in"
-    error_code=$((error_code + $?))
-    printl "Completed IOR: $IOR_API"
+        $IOR_EXE \
+            -H \
+            $IOR_CLI_ARGS \
+            -s 8 \
+            -o "$OUT_FILE" \
+            -f "${TOKIO_JOB_DIR}/inputs/${IOR_API}1m2.in"
+    ec=$?
+    error_code=$((error_code + $ec))
+    printlog "Completed IOR: $IOR_API-$READ_OR_WRITE [ec=$ec]"
+
+    if [ "$IOR_API" == "mpiio" ]; then
+        unset MPICH_MPIIO_HINTS_DISPLAY
+        unset MPICH_MPIIO_STATS
+    fi
 }
 
 function clean_ior() {
@@ -46,24 +69,96 @@ function clean_ior() {
     # no cleanup necessary due to IOR's keepFile option being disabled
 }
 
+VPIC_EXE_DIR=${TOKIO_JOB_DIR}/bin
+
+function run_vpicio() {
+    VPIC_EXE="$1"
+    OUT_FILE="$2"
+    NPROCS="$3"
+
+    if [[ "$VPIC_EXE" =~ dbscan_read ]]; then
+        extra_args="-d /Step#0/x -d /Step#0/y -d /Step#0/z -d /Step#0/px -d /Step#0/py -d /Step#0/pz -f"
+    elif [[ "$VPIC_EXE" =~ vpicio_uni ]]; then
+        extra_args=""
+    else
+        printerr "Unknown VPIC exe [$VPIC_EXE]; not passing any extra CLI args" >&2
+        extra_args=""
+    fi
+
+    printlog "Submitting VPIC-IO: $VPIC_EXE"
+    runjob -n $NPROCS -p 16 --block $COBALT_PARTNAME --envs BGLOCKLESSMPIO_F_TYPE=0x47504653 --verbose=INFO : \
+        ${VPIC_EXE_DIR}/${VPIC_EXE} \
+            $extra_args \
+            "$OUT_FILE"
+    ec=$?
+    error_code=$((error_code + $ec))
+    printlog "Completed VPIC-IO: $VPIC_EXE [ec=$ec]"
+}
+
+function clean_vpicio() {
+    OUT_FILE="$2"
+    if [ ! -z "$OUT_FILE" ]; then
+        printlog "Deleting ${OUT_FILE}"
+        rm -rf ${OUT_FILE}
+    fi
+}
+
 ################################################################################
 ###  IOR - MPI-IO shared-file and POSIX file-per-process
 ################################################################################
-echo "$IOR_PARAM_SETS" | while read parameters
-do
-    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]
-    then
+
+IOR_PARAMS_FILE="${TOKIO_JOB_DIR}/inputs/ior-mira.params"
+if [ ! -f "$IOR_PARAMS_FILE" ]; then
+    printerr "IOR_PARAMS_FILE=[$IOR_PARAMS_FILE] not found"
+    IOR_PARAMS_FILE=/dev/null
+fi
+PARAM_LINES=()
+while read -r parameters; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+        continue
+    fi
+    PARAM_LINES+=("$parameters")
+done <<< "$(IOR_OUT_DIR="${TOKIO_OUT_DIR}" envsubst < "$IOR_PARAMS_FILE")"
+for parameters in "${PARAM_LINES[@]}"; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
         continue
     fi
     run_ior $parameters
 done
-echo "$IOR_PARAM_SETS" | while read parameters
-do
-    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]
-    then
+for parameters in "${PARAM_LINES[@]}"; do
+    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
         continue
     fi
     clean_ior $parameters
 done
+
+################################################################################
+###  VPIC-IO - Write and read using HDF5 shared file (VPIC-IO and BD-CATS-IO)
+################################################################################
+
+#VPICIO_PARAMS_FILE="${TOKIO_JOB_DIR}/inputs/vpicio-mira.params"
+#if [ ! -f "$VPICIO_PARAMS_FILE" ]; then
+#    printerr "VPICIO_PARAMS_FILE=[$VPICIO_PARAMS_FILE] not found"
+#    VPICIO_PARAMS_FILE=/dev/null
+#fi
+#PARAM_LINES=()
+#while read -r parameters; do
+#    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+#        continue
+#    fi
+#    PARAM_LINES+=("$parameters")
+#done <<< "$(VPICIO_OUT_DIR="${TOKIO_OUT_DIR}" envsubst < "$VPICIO_PARAMS_FILE")"
+#for parameters in "${PARAM_LINES[@]}"; do
+#    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+#        continue
+#    fi
+#    run_vpicio $parameters
+#done
+#for parameters in "${PARAM_LINES[@]}"; do
+#    if [ -z "$parameters" ] || [[ "$parameters" =~ ^# ]]; then
+#        continue
+#    fi
+#    clean_vpicio $parameters
+#done
 
 return $error_code
