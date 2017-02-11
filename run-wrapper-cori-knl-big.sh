2b759c7fbc4c5cfcc609287e5a36027820af7f9d
Glenn K. Lockwood <glock@lbl.gov>
2017-02-10 16:17:15 -0800

    add big job size options; break edison out into per-file system tests

---
+++ run-wrapper-cori-knl-big.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# This submit script is intended to be submitted from the repository's base dir.
+#
+#SBATCH -J tokio-abc-cori-knl
+#SBATCH -N 256
+#SBATCH -p debug
+#SBATCH -t 00:30:00
+#SBATCH -C knl
+#SBATCH --mail-user=glock@lbl.gov
+#SBATCH --mail-type=END
+#DW jobdw type=scratch access_mode=striped capacity=7TiB pool=sm_pool
+#DW jobdw type=scratch access_mode=private capacity=7TiB pool=sm_pool
+
+i=0
+while [ -d runs.$i ]; do
+    let "i++"
+done
+echo "[$(date)] Outputting to runs.$i"
+
+export REPO_BASE_DIR="${SLURM_SUBMIT_DIR}"
+export TOKIO_LOGPATH="${SLURM_SUBMIT_DIR}/runs.$i"
+export DARSHAN_LOGPATH="${TOKIO_LOGPATH}"
+export TOKIO_PARAMS_FILE="${REPO_BASE_DIR}/inputs/${NERSC_HOST}-big.params"
+
+mkdir -p "$TOKIO_LOGPATH" && cd "$TOKIO_LOGPATH"
+../run-cron-benchmarks-nersc.sh
